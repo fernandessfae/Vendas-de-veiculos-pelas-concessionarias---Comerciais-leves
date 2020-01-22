9ebd0a209d3152a4f137976790e845a6339bc77f
Aristóteles Fernandes <fernandessfae@hotmail.com>
2020-01-22 16:04:27 -0300

    Série Temporal - Previsão Holt Winters

---
+++ série temporal - previsão com Holt Winters (aditivo).py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pylab as plt
+
+base = pd.read_csv('bcdata.sgs.7385.csv', delimiter = ';')
+
+dateparse = lambda dates: pd.datetime.strptime(dates, '%d/%m/%Y')
+base = pd.read_csv('bcdata.sgs.7385.csv', delimiter = ';', dayfirst = True,
+                   parse_dates = ['data'], index_col ='data', date_parser = dateparse)
+
+from statsmodels.tsa.api import ExponentialSmoothing
+
+fit1 = ExponentialSmoothing(base, seasonal_periods = 12, trend = 'additive', seasonal = 'additive').fit(use_boxcox = True)
+
+plt.figure(figsize = (10, 5))
+plt.xlabel('Anos')
+plt.ylabel('Quantidade de comerciais leves vendidos')
+fit1.fittedvalues.plot(style = '--', color = 'red')
+
+plt.figure(figsize = (10, 5))
+plt.xlabel('Anos')
+plt.ylabel('Quantidade de comerciais leves vendidos (previsão)')
+fit1.forecast(24).plot(style = '--', marker = 'o', color = 'black', label = 'Previsão', legend = True)
+
+plt.figure(figsize = (10, 5))
+plt.xlabel('Anos')
+plt.ylabel('Quantidade de comerciais leves vendidos')
+fit1.fittedvalues.plot(style = '--', color = 'red', legend = True, label = 'Dados Originais')
+fit1.forecast(24).plot(style = '--', color = 'black', legend = True, label = 'Previsão')
